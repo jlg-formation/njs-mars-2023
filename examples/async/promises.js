46775e8977afc615cb64d2816b92235b6a9f38a3
JLG <jlguenego@gmail.com>
2023-03-08 09:39:16 +0100

    :egg: use native promises

--- examples/async/promises.js
+++ examples/async/promises.js
@@ -1,26 +1,9 @@
-const fs = require("node:fs");
+const fs = require("node:fs").promises;
 
-const promisify = (func) => {
-  return (...args) => {
-    return new Promise((resolve, reject) => {
-      func(...args, (err, result) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(result);
-      });
-    });
-  };
-};
-
-const readdir = promisify(fs.readdir);
-const readFile = promisify(fs.readFile);
-
-readdir(".")
+fs.readdir(".")
   .then((files) => {
     console.log("files: ", files);
-    return readFile(files[0], { encoding: "utf-8" });
+    return fs.readFile(files[0], { encoding: "utf-8" });
   })
   .then((content) => {
     console.log("content: ", content);
